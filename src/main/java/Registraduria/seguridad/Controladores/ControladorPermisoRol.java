b4ea5c7c72b23019fe73b830a5e9b562a3d58ee4
Juanse1108 <juansetonog@gmail.com>
2022-11-05 19:36:47 -0500

    Actualizacion de controladores

--- src/main/java/Registraduria/seguridad/Controladores/ControladorPermisoRol.java
+++ src/main/java/Registraduria/seguridad/Controladores/ControladorPermisoRol.java
@@ -7,9 +7,12 @@ import Registraduria.seguridad.Repositorios.RepositorioPermiso;
 import Registraduria.seguridad.Repositorios.RepositorioPermisoRol;
 import Registraduria.seguridad.Repositorios.RepositorioRol;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @CrossOrigin
@@ -23,41 +26,88 @@ public class ControladorPermisoRol {
     @Autowired
     private RepositorioPermiso RepositorioPermiso;
 
-
-
-    @PostMapping("/rol/{idRol}/permiso/{idPermiso}")
-    public PermisoRol crearPermisoRol(@PathVariable String idRol,@PathVariable String idPermiso){
-
-        Rol rol = RepositorioRol.findById(idRol).orElseThrow(RuntimeException::new);
-
-        Permiso permiso = RepositorioPermiso.findById(idPermiso).orElseThrow(RuntimeException::new);
-
-        PermisoRol permisoRol = new PermisoRol(rol, permiso);
-
-        return RepositorioPermisosRol.save(permisoRol);
-
-    }
-    @GetMapping
-    public List<PermisoRol> listaPermisosRol(){
-
-        return RepositorioPermisosRol.findAll();
-
-    }
-
-    @GetMapping("/validar-permiso/rol/{idRol}")
-    public PermisoRol validarPermisosDelRol(@PathVariable String idRol, @RequestBody Permiso infoPermiso){
-        Rol rolActual =RepositorioRol.findById(idRol).orElse(null);
-        Permiso permisoActual= RepositorioPermiso.getPermiso(infoPermiso.getUrl(),infoPermiso.getMetodo());
-
-
-        if(rolActual != null && permisoActual !=null){
-            String idRolActual =rolActual.get_id();
-            String idPermisoActual= permisoActual.get_id();
-            PermisoRol permisosRolActual = RepositorioPermisosRol.findByRolAndPermissions(rolActual.get_id(), permisoActual.get_id());
-            return  permisosRolActual;
-        }else{
+    //Asignación rol y permiso
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("rol/{id_rol}/permiso/{id_permiso}")
+    public PermisoRol crearPermisoRol(@PathVariable String id_rol, @PathVariable String id_permiso) {
+        PermisoRol nuevo = new PermisoRol();
+        Rol rol = RepositorioRol.findById(id_rol).orElseThrow(RuntimeException::new);
+        Permiso permiso = RepositorioPermiso.findById(id_permiso).orElseThrow(RuntimeException::new);
+        if (rol != null && permiso != null) {
+            nuevo.setPermiso(permiso);
+            nuevo.setRol(rol);
+            return RepositorioPermisosRol.save(nuevo);
+        } else {
             return null;
         }
+    }
+
+    @GetMapping
+    public List<PermisoRol> listaPermisosRol(){
+        return RepositorioPermisosRol.findAll();
+    }
+
+    @GetMapping("{id}")
+    public PermisoRol show(@PathVariable String id) {
+        PermisoRol permisoRolActual = RepositorioPermisosRol
+                .findById(id)
+                .orElse(null);
+        return permisoRolActual;
+    }
+    //modificacion permiso rol
+    @PutMapping("{id}/rol/{id_rol}/permiso/{id_permiso}")
+    public PermisoRol update(@PathVariable String id, @PathVariable String id_rol, @PathVariable String id_permiso) {
+        PermisoRol permisoRolActual = RepositorioPermisosRol
+                .findById(id)
+                .orElse(null);
+        Rol elRol = RepositorioRol.findById(id_rol).get();
+        Permiso elPermiso = RepositorioPermiso.findById(id_permiso).get();
+        if (permisoRolActual != null && elPermiso != null && elRol != null) {
+            permisoRolActual.setPermiso(elPermiso);
+            permisoRolActual.setRol(elRol);
+            return RepositorioPermisosRol.save(permisoRolActual);
+        } else {
+            return null;
+        }
+    }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id) {
+        PermisoRol permisoRolActual = RepositorioPermisosRol
+                .findById(id)
+                .orElse(null);
+        if (permisoRolActual != null) {
+            RepositorioPermisosRol.delete(permisoRolActual);
+        }
+    }
+
+    @GetMapping("validar-permiso/rol/{id_Rol}")
+    public PermisoRol validarPermisosDelRol(@PathVariable String idRol, @RequestBody Permiso infoPermiso, HttpServletResponse response) throws IOException {
+
+        //Buscar en base de datos el rol y permiso
+        Rol rolActual = RepositorioRol.findById(idRol).orElse(null);
+        Permiso permisoActual = RepositorioPermiso.getPermiso(infoPermiso.getUrl(), infoPermiso.getMetodo());
+
+        //Validar si existe el rol y el permiso en base de datos
+        if (rolActual != null && permisoActual != null) {
+
+            String idRolActual = rolActual.get_id();
+            String idPermisoActual = permisoActual.get_id();
+
+            //Buscar en la tabla PermisosRol si el rol tiene asociado el permiso.
+            PermisoRol permisosRolActual = RepositorioPermisosRol.findByRolAndPermissions(idRolActual, idPermisoActual);
+
+            if (permisosRolActual != null) {
+                return permisosRolActual;
+            } else {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+                return null;
+            }
+        } else {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            return null;
+        }
+
     }
 
 
